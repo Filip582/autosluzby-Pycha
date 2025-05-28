d5def1fd7e316b1e831b575b7efebaa9486bfab8
filip patrman <filip.patrman.023@pslib.cz>
2025-05-28 14:45:04 +0200

    Refactor code structure for improved readability and maintainability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
+import Accordion from "accordion-js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  let hamelmnts = document.querySelectorAll(".hamburger-zone");
+  for (const btn of document.querySelectorAll(".hamburger-btn")) {
+      btn.addEventListener("click", (e) => {
+          e.preventDefault();
+          for (const element of hamelmnts) {
+              element.classList.toggle("active");
+          }  
+      });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ...tvůj kód pro hamburger...
+
+  
+  new Accordion('.accordion', {
+    duration: 300,
+    showMultiple: false
+  });
+});
