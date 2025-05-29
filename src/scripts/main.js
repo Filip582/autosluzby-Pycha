b6d7f7162b7ca643fcf7ccd5ebae5bbf1acf77e3
filip patrman <filip.patrman.023@pslib.cz>
2025-05-29 14:27:43 +0200

    Add GSAP for animations; update package.json and package-lock.json; enhance styles and structure for experience section and logo responsiveness

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 import Accordion from "accordion-js";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
 document.addEventListener("DOMContentLoaded", () => {
   let hamelmnts = document.querySelectorAll(".hamburger-zone");
   for (const btn of document.querySelectorAll(".hamburger-btn")) {
@@ -17,7 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-  // ...tvůj kód pro hamburger...
 
   
   new Accordion('.accordion', {
@@ -31,4 +33,46 @@ document.addEventListener("DOMContentLoaded", () => {
   if (yearSpan) {
     yearSpan.textContent = new Date().getFullYear();
   }
+
+  gsap.to(".expirience__image:nth-child(1)", {
+    x: "-20%",
+    scrollTrigger: {
+      trigger: ".expirience__image:nth-child(1)",
+      start: "top 100%",
+      end: "center center",
+      scrub: 2,
+    },
+  })
+  gsap.to(".expirience__image:nth-child(3)", {
+    x: "20%",
+    scrollTrigger: {
+      trigger: ".expirience__image:nth-child(3)",
+      start: "top 100%",
+      end: "center 60%",
+      scrub: 2,
+    },
+  })
+
+  gsap.to(".expirience__wheel", {
+    rotate: 360,
+    scrollTrigger: {
+      trigger: ".expirience__wheel",
+      start: "top 100%",
+      end: "bottom 0%",
+      scrub: 2,
+    },
+  })
+
+  gsap.to(".question__wheel", {
+    rotate: 360,
+    scrollTrigger: {
+      trigger: ".question__wheel",
+      start: "top 100%",
+      end: "bottom 0%",
+      scrub: 2,
+    },
+  })
+
 });
+
+
